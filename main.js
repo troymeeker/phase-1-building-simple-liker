4cad32d210c6829341ae1b6ee3abbb4e57c30b90
troymeeker <troymeeker1@gmail.com>
2021-08-01 13:18:40 -0700

    complete

--- main.js
+++ main.js
@@ -3,6 +3,46 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const modal = document.querySelector("div#modal")
+modal.className = "hidden"
+
+document.addEventListener("DOMContentLoaded", () => {
+  const modal = document.querySelector('#modal')
+  const hearts = document.querySelectorAll('span.like-glyph')
+  likepost(hearts);
+})
+
+
+
+const likepost = (hearts) => {
+  for(const heart of hearts){
+    heart.addEventListener('click', (e) => {
+    // make a server call
+      mimicServerCall()
+      .then(() => {
+      if(heart.innerHTML == EMPTY_HEART){
+        heart.innerHTML = FULL_HEART
+        heart.className = "activated-heart"
+      } else{ 
+        heart.innerHTML = EMPTY_HEART
+        heart.className  = "like-glyph"
+      }
+    })
+  .catch(error =>{
+    modal.hidden = false
+    const modalMessage = document.querySelector("#modal-message")
+    modalMessage.innerText = error
+    setTimeout(()=>{
+      modal.hidden = true
+    },3000)
+  })
+})
+  }
+}
+
+
+  
+
 
 
 
